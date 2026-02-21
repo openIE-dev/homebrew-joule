734661f80fae1867da1f9a6da58eb2e931a421c6
David Charlot <david@openie.dev>
2026-02-21 17:37:03 -0500

    Update joule formula to v1.1.6

--- Formula/joule.rb
+++ Formula/joule.rb
@@ -2,33 +2,41 @@ class Joule < Formula
   desc "The Joule programming language compiler - energy-aware systems programming"
   homepage "https://joule-lang.org"
   license "Apache-2.0"
-  version "1.0.0"
+  version "1.1.6"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/openIE-dev/joule-lang/releases/download/v#{version}/joule-macos-arm64.tar.gz"
-      sha256 "305eaeecddf8ed8469d3afbdfd70170df9582409d205526009fd4e1027d670bc"
+      sha256 "369009d1b4b14b1fc35e4d3d86d8a206b89dcb0b183f2347a1c60259f90c343e"
     else
       url "https://github.com/openIE-dev/joule-lang/releases/download/v#{version}/joule-macos-x86_64.tar.gz"
-      sha256 "fa031a8858fe28014442dcfa0c100c59385b544c2e7b182ca466c5eeaa5367d3"
+      sha256 "7cd314ca48cdd50bb6d23f61480f6ecfad641a8317afbc5aff869edd6f0e5174"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/openIE-dev/joule-lang/releases/download/v#{version}/joule-ubuntu-arm64.tar.gz"
-      sha256 "94f0cfe07aa5e27e03204dea8da7d67ffbec208597684ac7bcf028e71c08a555"
+      sha256 "a74296c53796335a5b96fbb092b8007c9b29bf6d240ed54912428c50a2047565"
     else
       url "https://github.com/openIE-dev/joule-lang/releases/download/v#{version}/joule-ubuntu-x86_64.tar.gz"
-      sha256 "9530524e765f61d824d0c9763c214d90dd9db57ee0ac8776f8b64b40017b35f2"
+      sha256 "10700aa43cabd9c444cf403fa476da9613ce35b8eb747a90b6000aab32ca05a2"
     end
   end
 
   def install
+    bin.install "bin/joule"
     bin.install "bin/joulec"
+    bin.install "bin/amp"
+    bin.install "bin/joule-lsp"
+    bin.install "bin/joule-debug"
+    bin.install "bin/joule-fmt"
+    bin.install "bin/joule-audit"
+    bin.install "bin/joule-doc"
   end
 
   test do
     assert_match "joulec", shell_output("#{bin}/joulec --version")
+    assert_match "joule", shell_output("#{bin}/joule --version")
   end
 end
